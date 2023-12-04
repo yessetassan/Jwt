b9d6f1023bc984243feabd75793e1ecec694aa0c
yessetassan <139701904+yessetassan@users.noreply.github.com>
2023-12-04 21:34:12 +0600

    Dockerfile added !

--- Jwt/src/main/java/com/example/project/Jwt/controller/WelcomeController.java
+++ Jwt/src/main/java/com/example/project/Jwt/controller/WelcomeController.java
@@ -1,8 +1,16 @@
 package com.example.project.Jwt.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Secured !";
+    }
+
 }
